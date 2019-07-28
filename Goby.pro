b847fc64a9f925645725735f1f97744c33588585
Jon Graham <JonCG90@gmail.com>
2019-07-28 22:02:00 +1200

    Move files to src directory

--- Goby.pro
+++ Goby.pro
@@ -25,7 +25,7 @@ DEFINES += QT_DEPRECATED_WARNINGS
 CONFIG += c++11
 
 SOURCES += \
-        main.cpp \
+        src/main.cpp \
         src/ui/mainWindow.cpp
 
 HEADERS += \
--- moc_openglwindow.cpp
+++ moc_openglwindow.cpp
@@ -6,7 +6,7 @@
  ** WARNING! All changes made in this file will be lost!
  *****************************************************************************/
 
-#include "openglwindow.hpp"
+#include "src/ui/openglwindow.hpp"
 #include <QtCore/qbytearray.h>
 #include <QtCore/qmetatype.h>
 #if !defined(Q_MOC_OUTPUT_REVISION)
